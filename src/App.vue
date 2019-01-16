771b4e0a2dab62cd49a32f68ce046a3a28abf394
Lin Zi Yang <17600273758@163.com>
2019-01-16 16:33:55 +0800

    背景图

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  html, body {
+    font-size: 50%;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    background: url("../static/images/bg.jpg") no-repeat;
+    background-size: 100% 100%;
+  }
+  #app {
+    overflow: hidden;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+  }
+  /*img {*/
+    /*width: 100%;*/
+    /*height: 100%;*/
+    /*opacity: 0.5;*/
+  /*}*/
+</style>
