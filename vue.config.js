1ccb002ab745974a8ad54bfaa8362ce959335a53
V0001630 <17600273758@163.com>
2023-07-17 15:45:26 +0800

    自动化部署1

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = defineConfig({
-  publicPath: '/',
+  publicPath: '/gh-pages/',
+  outputDir: 'dist', // 构建时的输出目录
+  assetsDir: './static', // 放置静态资源的目录
+  indexPath: 'index.html', // html 的输出路径
   configureWebpack: config => {
     config.externals = {
       AMap: 'AMap'
